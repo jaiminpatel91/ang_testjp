41a4b5954bab8a5174c5a64107693aa74d2d6237
Admin <admin@Jaimin-Mac.fios-router.home>
2017-05-16 19:49:29 -0400

    no duplication in task added.

--- app/todo.component.js
+++ app/todo.component.js
@@ -13,17 +13,34 @@ var todoComponent = (function () {
     function todoComponent() {
         this.newTodo = '';
         this.todos = [];
+        this.size = 0;
     }
     todoComponent.prototype.addTodo = function () {
-        this.todoObj = {
-            newTodo: this.newTodo,
-            completed: false
-        };
-        this.todos.push(this.todoObj);
-        this.newTodo = '';
+        this.duplicate = false;
+        if (this.newTodo.length >= 1) {
+            for (var i = 0; i <= this.todos.length; i++) {
+                if (this.todos[i] === this.newTodo) {
+                    this.duplicate = true;
+                    alert("this task is alredy in your list");
+                    this.newTodo = '';
+                }
+            }
+            if (this.duplicate === false) {
+                this.todos.push(this.newTodo);
+                this.newTodo = '';
+                this.sizeTodo();
+            }
+        }
+        else {
+            alert("please enter task.");
+        }
+    };
+    todoComponent.prototype.sizeTodo = function () {
+        this.size = this.todos.length;
     };
     todoComponent.prototype.deleteTodo = function (index) {
         this.todos.splice(index, 1);
+        this.sizeTodo();
     };
     return todoComponent;
 }());
--- app/todo.component.ts
+++ app/todo.component.ts
@@ -10,22 +10,43 @@ export class todoComponent{
     newTodo: string;
     todos: any;
     todoObj: any;
+    size: any;
+    date: any;
+    duplicate: Boolean;
 
     constructor(){
         this.newTodo = '';
         this.todos = [];
+        this.size = 0; 
     }
 
     addTodo() {
-        this.todoObj = {
-            newTodo: this.newTodo,
-            completed: false
+        this.duplicate = false;
+        if(this.newTodo.length >= 1){
+        for(let i=0; i<=this.todos.length; i++){
+            if(this.todos[i] === this.newTodo){
+                this.duplicate = true;
+                alert("this task is alredy in your list");
+                this.newTodo = '';
+            }
         }
-        this.todos.push(this.todoObj);
-        this.newTodo = '';
+        if(this.duplicate === false){
+                this.todos.push(this.newTodo);
+                this.newTodo = '';
+                this.sizeTodo(); 
+        }          
+        }
+        else{
+            alert("please enter task.");
+        }
+        
     }
-
+    sizeTodo() {
+        this.size = this.todos.length;
+    }
+    
     deleteTodo(index) {
       this.todos.splice(index, 1);
+      this.sizeTodo();
     }
 }
